9546b397b1dd122dd9d2a5c5aeac2188bbb33b4a
DaffaPintu <daffa@pintu.co.id>
2024-01-13 13:32:48 +0700

    fix error

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -2,16 +2,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Selenium {
 
+    public static WebDriver driver;
+
     @Test
     public void helloJayjay() {
-        WebDriver driver = WebDriverManager.chromedriver().create();
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://jayjay.co/");
         String text = driver.findElement(By.className("first-course")).getText();
         assertEquals("Gebrakan Kursus Online yang Siap Meroketkan Kariermu", text);
+        driver.quit();
     }
 }
